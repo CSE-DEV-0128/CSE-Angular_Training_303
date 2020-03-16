911109e8304e6106d8a6fcff4bfbb01fa4c27609
CSE-DEV-0128 <62118075+CSE-DEV-0128@users.noreply.github.com>
2020-03-16 14:04:03 -0500

    Add files via upload

---
+++ _async-pipe.md
@@ -0,0 +1,4 @@
+The source code for "async-pipe" is published online at:
+
+https://github.com/AngularBootCamp/async-pipe
+
---
+++ src/app/employee-loader.service.ts
@@ -0,0 +1,24 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+const apiUrl = 'https://api.angularbootcamp.com';
+
+export interface Employee {
+  first_name: string;
+  last_name: string;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class EmployeeLoaderService {
+  constructor(private http: HttpClient) {}
+
+  loadEmployees(): Observable<Employee[]> {
+    return this.http
+      .get<Employee[]>(apiUrl + '/employees')
+      .pipe(map(employees => employees.slice(0, 5)));
+  }
+}
